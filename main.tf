102802764256bf009886e643883c076771a686be
David Arnold <dja+djaboxx@sonic.net>
2018-10-03 15:45:37 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
-//--------------------------------------------------------------------
-// Variables
+variable "gcp_credentials" {
+  description = "GCP credentials needed by google provider"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "gcp_region" {
+ description = "GCP region"
+ default = "us-east1"
+}
+
+// Google Provider Configuration
+provider "google" {
+  credentials = "${var.gcp_credentials}"
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
 variable "compute_instance_count" {}
 variable "compute_instance_disk_image" {}
 variable "compute_instance_disk_size" {}
@@ -51,3 +69,22 @@ module "network" {
   description = "Darnold Demo Network"
   name = "gcp-demo"
 }
+  
+// Terraform outputs
+output "network_name" {
+  value = "${module.network.name}"
+}
+  
+output "subnet_gateway_address" {
+  value = "${module.network_subnet.gateway_address}"
+} 
+  
+output "firewall_self_link" {
+  value = "${module.network_firewall.self_link}"
+}
+  
+output "compute_instance_addresses" {
+  value = "${module.compute_instance.addresses}"
+
+
+
